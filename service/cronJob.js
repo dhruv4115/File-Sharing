c6a295de14d70c63e0432a0c1a29b39a2123a91f
Dhruv Tiwari <tiwaridhruv4146@gmail.com>
2025-07-18 00:18:18 +0530

    setting up cronJob

--- service/cronJob.js
+++ service/cronJob.js
@@ -10,3 +10,7 @@ function ExpiryCron(){
 
 module.exports = ExpiryCron;
 
+/*1.Build THe Features
+2. Validate the uploaded files and return approppriate response message along with status code
+3. Complete code for cron job. SetExpiry -> (CHOOSE) Url expiry or File expiry Reason
+4.For Expiry you have options - 1 day, 2 day, 7 day, 31 days -> only date matters */
